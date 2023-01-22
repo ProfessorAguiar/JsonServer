81b4b3fcb1e72d4ce99b61fe6ae05806abfaadeb
Vinicius Aguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-01-22 20:03:19 -0300

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-import jsonServer from 'json-server'
-const server=jsonServer.create()
-const router=jsonServer.router('notas.json')
-const middlewares=jsonServer.defaults()
-const port=process.env.PORT || 3200
-server.use(middlewares)
-server.use(router)
-server.listen(port)
